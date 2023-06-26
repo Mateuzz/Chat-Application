68018f7ab309102bff2978595c3907238ac8a1b7
Mateus <vactusorigin@gmail.com>
2023-06-26 17:08:36 -0300

    Corrigdo premake

--- premake5.lua
+++ premake5.lua
@@ -8,11 +8,13 @@ workspace "chat"
     targetdir "bin"
 
     filter "configurations:Release"
+        kind "WindowedApp"
         defines {"RELEASE"}
         optimize "On"
 
     filter "configurations:Debug"
         defines {"DEBUG"}
+        kind "ConsoleApp"
         symbols "On"
 
     filter {"platforms:Linux64"}
@@ -29,8 +31,6 @@ workspace "chat"
         architecture "x86"
 
     filter {}
-        kind "ConsoleApp"
-        -- kind "WindowedApp"
         libdirs {"lib"}
         links {"SDL2", "OpenGL", "GL", "GLEW", "m", "GLU"}
         includedirs {"src/", "vendor/"}
@@ -38,11 +38,3 @@ workspace "chat"
     project "main"
         files {"src/**.c", "src/**.h"}
         targetname "main"
-
-    project "server"
-        files {"demo/demo_server.c", "src/chat_server.c", "src/network.c"}
-        targetname "server"
-
-    project "client"
-        files {"demo/demo_client.c", "src/chat_client.c", "src/network.c"}
-        targetname "client"
